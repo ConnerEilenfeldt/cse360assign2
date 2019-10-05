acd06de22014fd37f59cf0dced21028a11fff0c5
Conner Eilenfeldt <ceilenfe@asu.edu>
2019-10-05 12:23:34 -0700

    Second Commit

--- cse360assign2/cse360assign2/AddingMachine.java
+++ cse360assign2/cse360assign2/AddingMachine.java
@@ -8,5 +8,52 @@ package cse360assign2;
 
 public class AddingMachine
 {
+	private int total = 0;
+	private String history = "0";
+	
+	/**
+	 * AddingMachine constructor
+	 */
+	public AddingMachine()
+	{
+		
+	}
+	
+	/**
+	 * add adds value to the total and records transaction in history
+	 * @param value the value to be added
+	 */
+	public void add(int value)
+	{
+		total += value;
+		history += " + " + value;
+	}
 
+	/**
+	 * subtract subtracts value from the total and records transaction in history
+	 * @param value the value to be subtracted
+	 */
+	public void subtract(int value)
+	{
+		total -= value;
+		history += " - " + value;
+	}
+	
+	/**
+	 * getTotal returns the current total
+	 * @return total
+	 */
+	public int getTotal()
+	{
+		return total;
+	}
+	
+	/**
+	 * toString returns the transaction history
+	 * @return history
+	 */
+	public String toString()
+	{
+		return history;
+	}
 }
